987b332809d7087c7debe6d0a5f39a2516e8cd40
Woodyhu96 <woodyhu@udel.edu>
2020-02-26 01:29:35 -0500

    Add game feature including background and gameObject

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -2,15 +2,54 @@ import ExampleObject from '../objects/exampleObject';
 
 export default class MainScene extends Phaser.Scene {
   private exampleObject: ExampleObject;
-
+  supreme: Phaser.GameObjects.Image;
+  adidas: Phaser.GameObjects.Image;
+  nike: Phaser.GameObjects.Image;
+  stockx: Phaser.GameObjects.Image;
   constructor() {
     super({ key: 'MainScene' });
   }
 
   create() {
-    this.exampleObject = new ExampleObject(this, 0, 0);
-  }
+    var random1 = Phaser.Math.Between(50, 1200);
+    var random2 = Phaser.Math.Between(50, 1200);
+    var random3 = Phaser.Math.Between(50, 1200);
+    var random4 = Phaser.Math.Between(50, 1200);
+    
+    this.add.image(600,400,"background");
+    this.supreme = this.add.image(random1, -300, "supreme");
+    this.supreme.setScale(0.5);
+    this.adidas = this.add.image(random2, -100, "adidas");
+    this.adidas.setScale(0.05);
+    this.nike = this.add.image(random3, 200, "nike");
+    this.nike.setScale(0.2);
+    this.stockx = this.add.image (random4, -500, "stockx");
+    this.stockx.setScale(0.5);
 
+    this.add.text(20,20, "Playing game", {font: "25px Arial", fill:"yellow"});
+    ///this.exampleObject = new ExampleObject(this, 0, 0);
+  }
+  moveicon(icon,speed,rotate){
+    icon.y += speed;
+    if(rotate === true){
+      icon.angle += 2;
+    }
+    if(icon.y > 1100){
+      this.resetIconPos(icon);
+    }
+  }
+  resetIconPos(icon){
+    icon.y = -50;
+    var randomx = Phaser.Math.Between(50, 1200);
+    icon.x = randomx;
+  }
+  respawn(num){
+    
+  }
   update() {
+      this.moveicon(this.nike, 1,false);
+      this.moveicon(this.supreme, 1,false);
+      this.moveicon(this.stockx, 1,true);
+      this.moveicon(this.adidas, 1,false);
   }
 }
--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -4,9 +4,18 @@ export default class PreloadScene extends Phaser.Scene {
   }
 
   preload() {
+    this.load.image("background","assets/background.jpg");
+    this.load.image("supreme", "assets/sup.png");
+    this.load.image("stockx","assets/stockx.png");
+    this.load.image("adidas","assets/adidas.png");
+    this.load.image("nike","assets/nike.png");
   }
 
   create() {
+    this.add.text(20,20,"Loading game",{
+      font: "25px Arial", 
+      fill:"yllow"
+    });
     this.scene.start('MainScene');
   }
 }
