9a4ab22cbf12e108b804e64d9780a86921104f7c
Woodyhu96 <woodyhu@udel.edu>
2020-03-06 15:42:17 -0500

    Updates

---
+++ beta/precache-manifest.144c81b73393a628220bf4818c85a331.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e03a6aa281baabe07582aad9a3f64ab",
+    "url": "/coding-2-try-phaser-Woodyhu96/betaassets/.DS_Store"
+  },
+  {
+    "revision": "67c506174d25a731bf868601238c54c7",
+    "url": "/coding-2-try-phaser-Woodyhu96/betaassets/background.jpg"
+  },
+  {
+    "revision": "3dc0089e68af6438d7ac72a7e430c589",
+    "url": "/coding-2-try-phaser-Woodyhu96/betaassets/citations.md"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "/coding-2-try-phaser-Woodyhu96/betaassets/font/font.png"
+  },
+  {
+    "revision": "38c02ad8ccaf9579150bb10d283e2b0c",
+    "url": "/coding-2-try-phaser-Woodyhu96/betaassets/font/font.xml"
+  },
+  {
+    "revision": "ccd0f68c9dc7cd68ea1e98bf43aa7c90",
+    "url": "/coding-2-try-phaser-Woodyhu96/betaassets/ship1.png"
+  },
+  {
+    "revision": "ea0ad2cda5a9dbbe332fd61e4c2f5700",
+    "url": "/coding-2-try-phaser-Woodyhu96/betaassets/ship2.png"
+  },
+  {
+    "revision": "5896d72a8a287a6e5f3272bfb9eadca8",
+    "url": "/coding-2-try-phaser-Woodyhu96/betaassets/ship3.png"
+  },
+  {
+    "revision": "86b88d1e4615b77a8b94a93ba82500bf",
+    "url": "/coding-2-try-phaser-Woodyhu96/betaassets/sounds/beam.mp3"
+  },
+  {
+    "revision": "ce33e3b835ac69e0766dbe6d7c67dfc1",
+    "url": "/coding-2-try-phaser-Woodyhu96/betaassets/sounds/beam.ogg"
+  },
+  {
+    "revision": "ce8dbd56bcb81632df7279e91fdcec54",
+    "url": "/coding-2-try-phaser-Woodyhu96/betaassets/sounds/explosion.mp3"
+  },
+  {
+    "revision": "4b9be0c2fdf194385275c2e8d006949b",
+    "url": "/coding-2-try-phaser-Woodyhu96/betaassets/sounds/explosion.ogg"
+  },
+  {
+    "revision": "2101f87289e14406eb4b215cf55a3ebc",
+    "url": "/coding-2-try-phaser-Woodyhu96/betaassets/sounds/pickup.mp3"
+  },
+  {
+    "revision": "2caf90e1d1576c0a7891bfb9723ded61",
+    "url": "/coding-2-try-phaser-Woodyhu96/betaassets/sounds/pickup.ogg"
+  },
+  {
+    "revision": "da087af3292fb062136a96eb496ceaf7",
+    "url": "/coding-2-try-phaser-Woodyhu96/betaassets/sounds/sci-fi_platformer12.mp3"
+  },
+  {
+    "revision": "240502f3ab4e1955dd49c1dbf827e247",
+    "url": "/coding-2-try-phaser-Woodyhu96/betaassets/sounds/sci-fi_platformer12.ogg"
+  },
+  {
+    "revision": "212424589cd39317c09de58252e2720f",
+    "url": "/coding-2-try-phaser-Woodyhu96/betaassets/spritesheets/.DS_Store"
+  },
+  {
+    "revision": "6be3462bcaa4a183b678800bb66d176c",
+    "url": "/coding-2-try-phaser-Woodyhu96/betaassets/spritesheets/beam.png"
+  },
+  {
+    "revision": "ea801e18a4f14b0c4740b555536b414e",
+    "url": "/coding-2-try-phaser-Woodyhu96/betaassets/spritesheets/explosion.png"
+  },
+  {
+    "revision": "ff70e55fbb2b70941d98b3e7ab1a01aa",
+    "url": "/coding-2-try-phaser-Woodyhu96/betaassets/spritesheets/player.png"
+  },
+  {
+    "revision": "05427110fd2c2de1d4738b31496a53c9",
+    "url": "/coding-2-try-phaser-Woodyhu96/betaassets/spritesheets/power-up.png"
+  },
+  {
+    "revision": "6387e4bedb194c28efe573c1cc5ce303",
+    "url": "/coding-2-try-phaser-Woodyhu96/betaassets/spritesheets/ship1.png"
+  },
+  {
+    "revision": "4888982b102ce1a7c57fb9f8c129e63d",
+    "url": "/coding-2-try-phaser-Woodyhu96/betaassets/spritesheets/ship2.png"
+  },
+  {
+    "revision": "15d30c88882683af9f5092c13dc13853",
+    "url": "/coding-2-try-phaser-Woodyhu96/betaassets/spritesheets/ship3.png"
+  },
+  {
+    "revision": "6a2cd0789ede499d790813fbe303c91c",
+    "url": "/coding-2-try-phaser-Woodyhu96/betafavicon.ico"
+  },
+  {
+    "revision": "6329340b3466e3f606a5",
+    "url": "/coding-2-try-phaser-Woodyhu96/betagen.main.3da1b621aefce17742b1.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-Woodyhu96/betagen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "fe60fe64236b97acc9589d1ed06bd2bf",
+    "url": "/coding-2-try-phaser-Woodyhu96/betaicons/icons-192.png"
+  },
+  {
+    "revision": "6a2cd0789ede499d790813fbe303c91c",
+    "url": "/coding-2-try-phaser-Woodyhu96/betaicons/icons-512.png"
+  },
+  {
+    "revision": "8918db271177952c694e0d7ba6a3514c",
+    "url": "/coding-2-try-phaser-Woodyhu96/betaindex.html"
+  },
+  {
+    "revision": "fc8cefcfb102b35601f09d51ee2512ef",
+    "url": "/coding-2-try-phaser-Woodyhu96/betamanifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "/coding-2-try-phaser-Woodyhu96/betasw.js"
+  }
+]);
